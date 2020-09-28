4c89e1b7a196a00e2fad6b80773243e1dbd1277b
Neelesh Shetty <neelesh@prographer.com>
2020-09-28 12:17:47 +0530

    some changes

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -4,7 +4,7 @@ import { Card } from 'antd';
 const Cards = (props) => {
     return (
         <div className="site-card-border-less-wrapper" >
-            <Card title={props.subject} bordered={false} style={{ width: "50vw", height: '100vh' }}>
+            <Card title={props.subject} bordered={false} style={{ height: '100vh' }}>
                 <p> {props.body}</p>
             </Card>
         </div>
